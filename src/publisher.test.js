f8277123ec8ad91cd5f02dca324cfb0eeaf6c09b
Daniel Merrell <daniel.r.merrell@gmail.com>
2023-10-13 14:04:56 -0600

    ref: fix whitespace

--- src/publisher.test.js
+++ src/publisher.test.js
@@ -1,30 +1,30 @@
 import { Publisher } from "./publisher";
 
 describe('Publisher', () => {
-    it('has a value', () => {
-        const publisher = new Publisher('2')
+  it('has a value', () => {
+    const publisher = new Publisher('2')
 
-        expect(publisher.value).toEqual('2');
+    expect(publisher.value).toEqual('2');
+  });
+
+  it('updates the value', () => {
+    const publisher = new Publisher('2')
+    expect(publisher.value).toEqual('2');
+
+    publisher.updateValue('3');
+
+    expect(publisher.value).toEqual('3');
+  });
+
+  it('publishes changes to subscribers', () => {
+    const subscriber = jest.fn();
+    const publisher = new Publisher('1');
+    publisher.addSubscription(subscriber);
+    publisher.updateValue('3');
+
+    expect(subscriber).toHaveBeenCalledWith({
+      oldValue: '1',
+      newValue: '3',
     });
-
-    it('updates the value', () => {
-        const publisher = new Publisher('2')
-        expect(publisher.value).toEqual('2');
-
-        publisher.updateValue('3');
-
-        expect(publisher.value).toEqual('3');
-    });
-
-    it('publishes changes to subscribers', () => {
-        const subscriber = jest.fn();
-        const publisher = new Publisher('1');
-        publisher.addSubscription(subscriber);
-        publisher.updateValue('3');
-
-        expect(subscriber).toHaveBeenCalledWith({
-            oldValue: '1',
-            newValue: '3',
-        });
-    })
+  })
 });
