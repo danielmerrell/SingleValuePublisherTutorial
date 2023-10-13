7bdb384f4a1317da45f9bf203c3c15df05ed90f9
Daniel Merrell <daniel.r.merrell@gmail.com>
2023-10-13 12:49:10 -0600

    spec: wrap in describe

--- src/manager.test.js
+++ src/manager.test.js
@@ -1,18 +1,20 @@
 import { Manager } from "./manager";
 
 describe('Manager', () => {
-  it('has a title', () => {
-    const manager = new Manager({ title: "I'm mr manager" });
+  describe('title', () => {
+    it('has a title', () => {
+      const manager = new Manager({ title: "I'm mr manager" });
 
-    expect(manager.title).toEqual("I'm mr manager");
-  })
+      expect(manager.title).toEqual("I'm mr manager");
+    })
 
-  it('can update the title', () => {
-    const manager = new Manager({ title: "initial" });
-    expect(manager.title).toEqual('initial');
+    it('can update the title', () => {
+      const manager = new Manager({ title: "initial" });
+      expect(manager.title).toEqual('initial');
 
-    manager.updateTitle('new title');
+      manager.updateTitle('new title');
 
-    expect(manager.title).toEqual('new title');
+      expect(manager.title).toEqual('new title');
+    });
   });
 });
